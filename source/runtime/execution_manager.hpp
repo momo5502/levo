286ba4182ba21e6a6ca5330ce967c61b4366c2aa
momo5502 <mauriceheumann@gmail.com>
2026-02-22 12:33:09 +0100

    Synchronize memory management

--- source/runtime/execution_manager.hpp
+++ source/runtime/execution_manager.hpp
@@ -6,15 +6,16 @@
 #include <span>
 #include <format>
 #include <stdexcept>
+#include <shared_mutex>
 
 namespace levo::runtime
 {
-    size_t align_down(const size_t value, const size_t alignment)
+    inline size_t align_down(const size_t value, const size_t alignment)
     {
         return value & ~(alignment - 1);
     }
 
-    size_t align_up(const size_t value, const size_t alignment)
+    inline size_t align_up(const size_t value, const size_t alignment)
     {
         return align_down(value + alignment - 1, alignment);
     }
@@ -24,6 +25,8 @@ namespace levo::runtime
       public:
         bool read(addr_t address, void* buffer, size_t size) override
         {
+            std::shared_lock lock(memory_mutex_);
+
             const auto memory = get_memory(address, size);
             if (memory.size() < size)
             {
@@ -36,6 +39,9 @@ namespace levo::runtime
 
         bool write(addr_t address, const void* buffer, size_t size) override
         {
+            // Maybe we should use a unique lock here?
+            std::shared_lock lock(memory_mutex_);
+
             const auto memory = get_memory(address, size);
             if (memory.size() < size)
             {
@@ -48,7 +54,8 @@ namespace levo::runtime
 
         void map(addr_t address, std::vector<uint8_t> data)
         {
-            mapped_memory_[address] = std::move(data);
+            std::unique_lock lock(memory_mutex_);
+            memory_[address] = std::move(data);
         }
 
         void map(addr_t address, size_t size)
@@ -58,22 +65,28 @@ namespace levo::runtime
 
         addr_t map_somewhere(size_t size)
         {
+            std::unique_lock lock(memory_mutex_);
+
             const auto address = find_free_address(size);
-            map(address, size);
+            memory_[address] = std::vector<uint8_t>(size, 0);
             return address;
         }
 
         void unmap(addr_t address)
         {
-            const auto it = mapped_memory_.find(address);
-            if (it != mapped_memory_.end())
+            std::unique_lock lock(memory_mutex_);
+
+            const auto it = memory_.find(address);
+            if (it != memory_.end())
             {
-                mapped_memory_.erase(it);
+                memory_.erase(it);
             }
         }
 
-        system_function* get_function(addr_t address) const
+        system_function* get_function(addr_t address)
         {
+            std::shared_lock lock(function_mutex_);
+
             auto it = functions_.find(address);
             if (it != functions_.end())
             {
@@ -84,6 +97,7 @@ namespace levo::runtime
 
         void add_function(addr_t address, system_function* function)
         {
+            std::unique_lock lock(function_mutex_);
             functions_[address] = function;
         }
 
@@ -100,16 +114,17 @@ namespace levo::runtime
         }
 
       private:
-        // TODO: Lock
+        std::shared_mutex memory_mutex_;
+        std::shared_mutex function_mutex_;
         std::map<addr_t, system_function*> functions_;
-        std::map<addr_t, std::vector<uint8_t>> mapped_memory_;
+        std::map<addr_t, std::vector<uint8_t>> memory_;
 
         addr_t find_free_address(const size_t size) const
         {
             constexpr addr_t allocation_granularity = 0x10000;
             addr_t last_end = allocation_granularity;
 
-            for (const auto& [address, data] : mapped_memory_)
+            for (const auto& [address, data] : memory_)
             {
                 if (address >= (last_end + size))
                 {
@@ -124,8 +139,8 @@ namespace levo::runtime
 
         std::span<uint8_t> get_memory(addr_t address, size_t size = 1)
         {
-            auto it = mapped_memory_.upper_bound(address);
-            if (it == mapped_memory_.begin())
+            auto it = memory_.upper_bound(address);
+            if (it == memory_.begin())
             {
                 return {};
             }
